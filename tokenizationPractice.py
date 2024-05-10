16d61664b13e52a08d6a2fd879b8de3dc508e890
Erik Cupsa <86483911+Erik-Cupsa@users.noreply.github.com>
2024-05-10 16:44:14 -0400

    Added notes to tokenizationPractice.py

--- tokenizationPractice.py
+++ tokenizationPractice.py
@@ -2,15 +2,15 @@ import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras.preprocessing.text import Tokenizer # type: ignore
 
-sentences = [
+sentences = [ #input sentences
     'i love my dog',
     'I, love my cat',
     'You love my dog!'
 ]
 
-tokenizer = Tokenizer(num_words = 100)
-tokenizer.fit_on_texts(sentences)
-word_index = tokenizer.word_index
-print(word_index)
-sequences = tokenizer.texts_to_sequences(sentences)
-print(sequences)
+tokenizer = Tokenizer(num_words = 100) ## will only keep the 100 most frequent words
+tokenizer.fit_on_texts(sentences) #updates internal vocabulary with sentences
+word_index = tokenizer.word_index #dictionary where keys are words, and values are index
+print(word_index) # {'love': 1, 'my': 2, 'i': 3, 'dog': 4, 'cat': 5, 'you': 6}
+sequences = tokenizer.texts_to_sequences(sentences) #converts sentences to sequences of characters, with each word replaced by its index
+print(sequences) #[[3, 1, 2, 4], [3, 1, 2, 5], [6, 1, 2, 4]]
